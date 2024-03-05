84db5039690268b4c54f3b8f1dd9e20d4715777d
nicholasboyce <nicholasboyce@protonmail.com>
2024-03-05 02:29:58 -0500

    Adjusted Vite build config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, 'client'),
+  base: '/social-links-profile-main/',
   build: {
     rollupOptions: {
       input: resolve(__dirname, 'client/index.html')
