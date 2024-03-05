46808184bbe2d4265a428bd7ca7674b3ce6241c2
nicholasboyce <nicholasboyce@protonmail.com>
2024-03-04 19:47:35 -0500

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: resolve(__dirname, 'client'),
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'client/index.html')
+    }
+  },
+  plugins: [react()],
+})
